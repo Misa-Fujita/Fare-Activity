879250ce4aef0309376c9f1c7e35d3a83221dd85
Misa-Fujita <165553372+Misa-Fujita@users.noreply.github.com>
2024-05-13 22:15:34 +0900

    Update index.php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-
+    <h2> This </h2>
     <form action="" method="POST" autocomplete="off" style="margin:10px">
         <label for="age">
             <input type="number" name="age" placeholder="age" style="margin-top:10px" required>
